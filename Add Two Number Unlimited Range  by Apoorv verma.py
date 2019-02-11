2311718bd8354b025ac58bceb17bf0161f70cc75
Apoorve Kumar verma <vapoorve@gmail.com>
2019-02-11 12:31:29 +0530

    Add files via upload

---
+++ Add Two Number Unlimited Range  by Apoorv verma.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Apoorv verma
+"""
+
+str1 = input("enter the number : ")
+str2 = input("enter the number : ")
+len1 ,len2 = len(str1),len(str2)
+maxval = max(len1,len2)
+str3 = "" 
+i = 0
+carry = 0
+while i != maxval :
+    if (len1 -1 - i) < 0:
+        num1 = 0
+    else:
+        num1 = str1[len1 -1 - i]
+    if (len2 -1 - i) < 0:
+        num2 = 0
+    else:
+        num2 = str2[len2 -1 - i]
+    d1 = int(num1)
+    d2 = int(num2)
+    if carry == 1 :
+       sum1 = d1 + d2 + 1
+    else:
+       sum1 = d1 + d2 
+    if (sum1) > 9:
+        amt = (sum1) - 10;carry = 1
+        str3 = str3 + str(amt)
+    else:
+        carry = 0
+        str3 = str3 + str(sum1)
+    i = i + 1
+print(str3[::-1])
